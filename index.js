0a6e23c53402d662814466d63597d1d47add2eb2
ndakalajulius <julius.ndakala@student.moringaschool>
2022-08-12 22:54:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(pple, event){
+  const watu = [];
+  for(let i = 0; i < pple.length; i++){
+      watu.push(`Thank you, ${pple[i]}, for the wonderful ${event} gift!`);
+  }
+  return watu ;
+}
+
+function countDown(num) {
+
+  while(num >= 0){
+      console.log(num);
+     num--;
+  }
+  
+}
